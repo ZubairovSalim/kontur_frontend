b4dbfbf7614f5ad6f65933842dd1126737e90382
Salim Zubairov <zubairov.salim@yandex.ru>
2019-02-19 01:11:27 +0500

    Сode refactoring

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const ERROR_MESSAGE = 'wrong command';
 
 app();
 
-function app () {
+function app() {
     const files = getFiles();
 
     console.log('Please, write your command!');
     readLine(processCommand, files);
 }
 
-function getFiles () {
+function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => {
         return {
@@ -22,10 +22,12 @@ function getFiles () {
     });
 }
 
-function processCommand (command, files) {
+function processCommand(command, files) {
     let allTODO = findTODO(files);
     let parsedTODO = parseTODO(allTODO);
 
+    //Разбиение введенной команды на 2 части, чтобы в примере: user Anonymous Developer,
+    //Anonymous Developer обрабатывалась как одна строка
     let mas = command.split(' ').filter(el => el !== "");
     let splittedCommand = [mas[0]];
     splittedCommand.push(mas.slice(1).join(' '));
@@ -54,7 +56,7 @@ function processCommand (command, files) {
                 break;
             }
             let date = new Date(splittedCommand[1]);
-            dataCommentTable(parsedTODO, date);
+            Comment.showCommentsTable(parsedTODO.filter(c => c.date >= date));
             break;
         case 'sort':
             if(splittedCommand[1] === '') {
@@ -70,12 +72,17 @@ function processCommand (command, files) {
     }
 }
 
-function findTODO (files) {
+/**
+ * Находит все необходимые комментарии
+ * @param files Массив объектов, содержащих 2 поля: имя исходного файла и считанный файл
+ * @returns {Array} Массив объектов, содержащих 2 поля: имя исходного файла и комментарий
+ */
+function findTODO(files) {
     let mas = [];
     let regex = /\/\/\s*todo(?!\s*:?\s*\n|$)\s*:?\s*(.*)$/igm;
 
     files.forEach(f => {
-        let matches = [];
+        let matches;
 
         while((matches = regex.exec(f.file)) !== null) {
             mas.push({
@@ -88,18 +95,22 @@ function findTODO (files) {
     return mas;
 }
 
+/**
+ * Возвращает массив распарсенных комментариев, упакованных в экземпляры класса Comment
+ * @param comments Массив исходных комментариев
+ * @returns {Array} Массив экземпляров класса Comment
+ */
 function parseTODO(comments) {
     let parsedComments = [];
 
     comments.forEach(c => {
-        let importance = "";
+        let importance;
         let user = "";
         let date = "";
         let parsedComment = "";
         let filename = c.filename;
 
-
-        importance = (c.comment.match(/!/g) || []).length;
+        importance = getImportanceLevel(c.comment);
 
         let arr = c.comment.split(';');
 
@@ -112,7 +123,10 @@ function parseTODO(comments) {
             parsedComment = c.comment;
         }
 
-        parsedComments.push(new Comment(importance, user, date, parsedComment, filename));
+        //исключаем пустые комментарии
+        if(user || date || parsedComment) {
+            parsedComments.push(new Comment(importance, user, date, parsedComment, filename));
+        }
     });
 
     return parsedComments;
@@ -128,135 +142,163 @@ class Comment {
         this.dateStr = date;
     }
 
+    /**
+     * Рассчитывает размеры колонок таблицы и отрисовывает ее
+     * @param Comments Массив комментариев
+     */
     static showCommentsTable(Comments) {
-        let col2Width = Math.max.apply(null, Comments.map(c => c.user.length));
-        let col3Width = Math.max.apply(null, Comments.map(c => c.dateStr.length));
-        let col4Width = Math.max.apply(null, Comments.map(c => c.comment.length));
-        let col5Width = Math.max.apply(null, Comments.map(c => c.filename.length));
+        const DELIMITERS = 4;
+        const INTERNAL_PADDINGS = 20;
+        const IMPORTANCE_WIDTH = 1;
 
-        col2Width = col2Width >= 4 && col2Width < 10  ? col2Width : col2Width < 4 ? 4 : 10;
-        col3Width = col3Width >= 4 && col3Width < 10  ? col3Width : col3Width < 4 ? 4 : 10;
-        col4Width = col4Width >= 7 && col4Width < 50  ? col4Width : col4Width < 7 ? 7 : 50;
-        col5Width = col5Width >= 8 && col5Width < 15  ? col5Width : col5Width < 8 ? 8 : 15;
+        let fields = ["user", "dateStr", "comment", "filename"];
+        let minColWidths = [4, 4, 7, 8];
+        let maxColWidths = [10, 10, 50, 15];
+        let colWidths;
+        let spaceCount;
 
-        let dif2 = (col2Width - 4) > 0 ? (col2Width - 4) : 0;
-        let dif3 = (col3Width - 4) > 0 ? (col3Width - 4) : 0;
-        let dif4 = (col4Width - 7) > 0 ? (col4Width - 7) : 0;
-        let dif5 = (col5Width - 8) > 0 ? (col5Width - 8) : 0;
+        colWidths = fields.map(field => Math.max.apply(null, Comments.map(c => c[field].length)));
 
-        let allWidth = 1 + col2Width + col3Width + col4Width + col5Width + 20 + 4;
+        colWidths = colWidths.map((width, i) => width >= minColWidths[i] && width < maxColWidths[i] ? width :
+            width < minColWidths[i] ? minColWidths[i] : maxColWidths[i]);
 
-        let header = "  !  |  user" + " ".repeat(dif2) + "  |  date" + " ".repeat(dif3) + "  |  comment" +
-            " ".repeat(dif4) + "  |  fileName" + " ".repeat(dif5) + "  \n";
+        spaceCount = colWidths.map((width, i) => (width - minColWidths[i]) > 0 ?
+            (width - minColWidths[i]) : 0);
 
-        header += "-".repeat(allWidth);
+        let allWidth = colWidths.reduce((sum, current) => sum + current, IMPORTANCE_WIDTH + INTERNAL_PADDINGS + DELIMITERS);
 
-        let content = Comments.reduce((coms, c) => {
-            let dif2 = (col2Width - c.user.length) > 0 ? (col2Width - c.user.length) : 0;
-            let dif3 = (col3Width - c.dateStr.length) > 0 ? (col3Width - c.dateStr.length) : 0;
-            let dif4 = (col4Width - c.comment.length) > 0 ? (col4Width - c.comment.length) : 0;
-            let dif5 = (col5Width - c.filename.length) > 0 ? (col5Width - c.filename.length) : 0;
-
-            let importance = c.importance > 0 ? "!" : " ";
-            let user = c.user.length > col2Width ? c.user.substring(0, col2Width-3) + "..." : c.user;
-            let date = c.dateStr.length > col3Width ? c.dateStr.substring(0, col3Width-3) + "..." : c.dateStr;
-            let comment = c.comment.length > col4Width ? c.comment.substring(0, col4Width-3) + "..." : c.comment;
-            let filename = c.filename.length > col5Width ? c.filename.substring(0, col5Width-3) + "..." : c.filename;
-
-            return coms + ("  " + importance + "  |  " + user + " ".repeat(dif2) + "  |  " + date + " ".repeat(dif3) +
-                "  |  " + comment + " ".repeat(dif4) + "  |  " + filename + " ".repeat(dif5) + "  \n");
-        }, header + "\n");
-
-        let footer = "-".repeat(allWidth);
-
-        Comments.length !== 0 ? console.log(content + footer) : console.log(content.substring(0, content.length - 1));
+        buildTable(Comments, fields, colWidths, spaceCount, allWidth);
     }
 }
 
-function dataCommentTable(parsedTODO, date) {
-    Comment.showCommentsTable(parsedTODO.filter(c => {
-        return c.date >= date;
-    }));
+/**
+ * Построение и вывод таблицы с комментариями
+ * @param Comments Массив комментариев
+ * @param fields Массив с полями таблицы
+ * @param colWidths Массив, содержащий размер содержимого колонок
+ * @param spaceCount Массив, содержащий отступы колонок
+ * @param allWidth Ширина таблицы
+ */
+function buildTable(Comments, fields, colWidths, spaceCount, allWidth) {
+    let header = "  !  |  user" + " ".repeat(spaceCount[0]) + "  |  date" + " ".repeat(spaceCount[1]) +
+        "  |  comment" + " ".repeat(spaceCount[2]) + "  |  fileName" + " ".repeat(spaceCount[3]) + "  \n";
+
+    header += "-".repeat(allWidth);
+
+    let content = Comments.reduce((coms, c) => {
+        let contentWidths;
+        let spaceContentCount;
+        let importance = c.importance > 0 ? "!" : " ";
+
+        spaceContentCount = colWidths.map((width, i) => (width - c[fields[i]].length) > 0 ?
+            (width - c[fields[i]].length) : 0);
+        contentWidths = colWidths.map((width, i) => c[fields[i]].length > width ?
+            c[fields[i]].substring(0, width - 3) + "..." : c[fields[i]]);
+
+        return coms + ("  " + importance + "  |  " + contentWidths[0] + " ".repeat(spaceContentCount[0]) + "  |  " +
+            contentWidths[1] + " ".repeat(spaceContentCount[1]) + "  |  " + contentWidths[2] +
+            " ".repeat(spaceContentCount[2]) + "  |  " + contentWidths[3] + " ".repeat(spaceContentCount[3]) + "  \n");
+    }, header + "\n");
+
+    let footer = "-".repeat(allWidth);
+
+    Comments.length !== 0 ? console.log(content + footer) : console.log(content.substring(0, content.length - 1));
 }
 
+/**
+ * Отображает отсортированную таблицу комментариев по определенному критерию
+ * @param parsedTODO Массив комментариев
+ * @param criterion Критерий сортировки
+ */
 function sortCommentTable(parsedTODO, criterion) {
     if(criterion === "importance") {
-        let sorted = parsedTODO.sort((a, b) => {
+        let sorted = parsedTODO.slice();
+        sorted.sort((a, b) => {
             if(a.importance < b.importance) {
                 return 1;
             }
-            if(a.importance > b.importance) {
+            else if(a.importance > b.importance) {
                 return -1;
             }
-
             return 0;
         });
 
         Comment.showCommentsTable(sorted);
     }
     else if(criterion === "user") {
-        let sorted = parsedTODO.sort((a, b) => {
-
-            if((a.user.length !== 0 && b.user.length !== 0 && a.user.toLowerCase() > b.user.toLowerCase()) ||
-                (a.user.length === 0 && b.user.length !== 0)) {
+        let sorted = parsedTODO.slice();
+        sorted.sort((a, b) => {
+            if((a.user && b.user && a.user.toLowerCase() > b.user.toLowerCase()) || (!a.user && b.user)) {
                 return 1;
             }
-
-            else if((a.user.length !== 0 && b.user.length !== 0 && a.user.toLowerCase() < b.user.toLowerCase()) ||
-                (a.user.length !== 0 && b.user.length === 0)) {
+            else if((a.user && b.user && a.user.toLowerCase() < b.user.toLowerCase()) || (a.user && !b.user)) {
                 return -1;
             }
-
-            else {
-                return 0;
-            }
+            return 0;
         });
 
         Comment.showCommentsTable(sorted);
     }
     else if(criterion === "date") {
-        let sorted = parsedTODO.sort((a, b) => {
-            if((!isNaN(a.date.valueOf()) && !isNaN(b.date.valueOf()) && a.date > b.date) ||
-                (!isNaN(a.date.valueOf()) && isNaN(b.date.valueOf()))) {
+        let sorted = parsedTODO.slice();
+        sorted.sort((a, b) => {
+            let a_date_num = a.date.getTime();
+            let b_date_num = b.date.getTime();
+
+            if((!isNaN(a_date_num) && !isNaN(b_date_num) && a.date > b.date) ||
+                (!isNaN(a_date_num) && isNaN(b_date_num))) {
                 return -1;
             }
-            else if ((!isNaN(a.date.valueOf()) && !isNaN(b.date.valueOf()) && a.date < b.date) ||
-                (isNaN(a.date.valueOf()) && !isNaN(b.date.valueOf()))) {
+            else if ((!isNaN(a_date_num) && !isNaN(b_date_num) && a.date < b.date) ||
+                (isNaN(a_date_num) && !isNaN(b_date_num))) {
                 return 1;
             }
-            else {
-                return 0;
-            }
+            return 0;
         });
 
         Comment.showCommentsTable(sorted);
     }
-    else {
-        console.log(ERROR_MESSAGE);
-    }
+    else console.log(ERROR_MESSAGE);
 }
 
+/**
+ * Проверка даты на валидность, для исключения ввода комманд типа: date -2018 или date 201803 и др.
+ * @param dateString Строка с датой
+ * @returns {boolean} Резльтат проверки: истина или ложь
+ */
 function dateIsCorrect(dateString) {
-    let dateTemplate = /\d{1,4}(-\d{1,2}){0,2}/;
-
+    let dateTemplate = /\d{4}(-\d{1,2}){0,2}/;
     let testDate = new Date(dateString);
 
-    if(testDate instanceof Date && !isNaN(testDate.valueOf())) {
-        return (dateString.match(dateTemplate)[0].length === dateString.length);
+    if(testDate instanceof Date && !isNaN(testDate.getTime())) {
+        let match = dateString.match(dateTemplate);
+        return (match !== null && match[0].length === dateString.length);
     }
     else return false;
 }
 
-//  todo
+/**
+ * Возвращает количество '!' знаков  в комментарии
+ * @param comment Комментарий
+ * @returns {number} Количество '!' знаков
+ */
+function getImportanceLevel(comment) {
+    let pos = -1;
+    let count = 0;
 
+    while ((pos = comment.indexOf("!", pos + 1)) !== -1) count++;
 
+    return count;
+}
+
+// todo ;;
+//todo
 // TODO you can do it!
-
 //TODO ff
-
 // TODO dexter;201803; докодить
-
 // TODO mer; 2018-03; сортировать даты
 // TODO tyre
 // TODO mers;-2018; полежать
-// TODO Anonymous;2019;temadat
+// TODO Anonymous;2019;temadat!!
+//todo user 2018; comment
+//todo : fox; 2009-02-20; try to use normalizr
